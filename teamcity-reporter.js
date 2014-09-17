8f9c3450f016ad77960158c1089cb03dfafde2a2
ilya-b <ilya-b@yandex-team.ru>
2014-09-17 17:30:24 +0400

    change code accordingly to google code style

--- teamcity-reporter.js
+++ teamcity-reporter.js
@@ -5,31 +5,32 @@ var util = require('util');
  * @param {Errors[]} errorsCollection
  */
 module.exports = function(errorsCollection) {
-    var errorCount = 0;
+  var errorCount = 0;
 
-    console.log('##teamcity[testSuiteStarted name=\'JSCS\']');
+  console.log('##teamcity[testSuiteStarted name=\'JSCS\']');
 
-    /**
-     * Formatting every error set.
-     */
-    errorsCollection.forEach(function(errors) {
-        var file = errors.getFilename();
+  /**
+   * Formatting every error set.
+   */
+  errorsCollection.forEach(function(errors) {
+    var file = errors.getFilename();
 
-        if (!errors.isEmpty()) {
-            errors.getErrorList().forEach(function(error) {
-                errorCount++;
-                console.log(util.format('##teamcity[testStarted name=\'%s\']', file));
-                console.log(util.format('##teamcity[testFailed name=\'%s\' message=\'line %d, col %d, %s\']',
-                    file, error.line, error.column, error.message));
-            });
-        }
-
-    });
-
-    if (errorCount === 0) {
-       console.log('##teamcity[testStarted name=\'JSCS\']');
-       console.log('##teamcity[testFinished name=\'JSCS\']');
+    if (!errors.isEmpty()) {
+      errors.getErrorList().forEach(function(error) {
+        errorCount++;
+        console.log(util.format('##teamcity[testStarted name=\'%s\']', file));
+        console.log(util.format(
+          '##teamcity[testFailed name=\'%s\' message=\'line %d, col %d, %s\']',
+          file, error.line, error.column, error.message));
+      });
     }
 
-    console.log(util.format('##teamcity[testSuiteFinished name=\'JSCS\']'));
+  });
+
+  if (errorCount === 0) {
+    console.log('##teamcity[testStarted name=\'JSCS\']');
+    console.log('##teamcity[testFinished name=\'JSCS\']');
+  }
+
+  console.log(util.format('##teamcity[testSuiteFinished name=\'JSCS\']'));
 };
